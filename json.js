bea08208113c22881b35894c0785e11072932e9f
harish paasu <paasuharish6@gmail.com>
2023-10-06 20:43:32 +0530

    first commit

---
+++ json.js
@@ -0,0 +1,9 @@
+let obj1 ={name: "person1", age:5};
+let obj2={age:5, name:"person1"};
+JSON.stringify(obj1)===JSON.stringify(obj2)
+if (obj1,obj2){
+    console.log("they are equal");
+
+}else{
+    console.log("they are not equal")
+}
